eb4eac7ed953b69e8a2fe583711a6fec72d6d68f
worthyMarqueyza <marqueyza@gmail.com>
2026-04-30 15:26:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return '''
-        <body style="background-color: black; color: #00FF00; font-family: 'Courier New', Courier, monospace; display: flex; justify-content: center; align-items: center; height: 100vh; margin: 0;">
-            <div>
-                <h1>> Hello, World_</h1>
-                <h2>> Questo è il mio progetto DevOps.</h2>
-            </div>
+        <body style="background-color: black; color: #00FF00; font-family: 'Droid Sans Mono', monospace; font-size: 16px; margin: 20px;">
+            Hello, World_
         </body>
     '''
 
