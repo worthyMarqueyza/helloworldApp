ea06c36489dda4053a7a11d77decdfe6efcf2ab0
worthyMarqueyza <marqueyza@gmail.com>
2026-04-30 14:53:29 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World! Questo è il mio progetto DevOps.'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
